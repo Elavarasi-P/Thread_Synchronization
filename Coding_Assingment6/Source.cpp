286de2306d43ffdd642dc3311e1dacf701b86efb
Elavarasi-P <elavarasi.p@hp.com>
2021-02-24 21:01:59 +0530

    Add project files.

---
+++ Coding_Assingment6/Source.cpp
@@ -0,0 +1,121 @@
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include<iostream>
+using namespace std;
+
+HANDLE ghWriteEvent;
+HANDLE ghThreads;
+string buff="";
+
+DWORD WINAPI ThreadProc(LPVOID lpParam)
+{
+
+    UNREFERENCED_PARAMETER(lpParam);
+
+    DWORD dwWaitResult=0;
+    int result = 1;
+    printf("Thread %d waiting for write event...\n", GetCurrentThreadId());
+
+    dwWaitResult = WaitForSingleObject(
+        ghWriteEvent, // event handle
+        INFINITE);    // indefinite wait
+
+    switch (dwWaitResult)
+    {
+    case WAIT_OBJECT_0:
+
+        printf("Thread %d reading from buffer\n",
+            GetCurrentThreadId());
+        cout<<"Content of buffer read:"<<buff<<"\n";
+        break;
+
+
+    default:
+        printf("Wait error (%d)\n", GetLastError());
+        result= 0;
+    }
+
+    printf("Reading Thread %d exiting\n", GetCurrentThreadId());
+    return result;
+}
+
+void CreateEventsAndThreads(void)
+{
+    
+    DWORD dwThreadID;
+
+    ghWriteEvent = CreateEvent(
+        NULL,               // default security attributes
+        TRUE,               // manual-reset event
+        FALSE,              // initial state is nonsignaled
+        TEXT("WriteEvent")  // object name
+    );
+
+    if (ghWriteEvent == NULL)
+    {
+        printf("CreateEvent failed (%d)\n", GetLastError());
+        return;
+    }
+
+    ghThreads = CreateThread(
+            NULL,              // default security
+            0,                 // default stack size
+            ThreadProc,        // name of the thread function
+            NULL,              // no thread parameters
+            0,                 // default startup flags
+            &dwThreadID);
+
+    if (ghThreads == NULL)
+    {
+            printf("CreateThread failed (%d)\n", GetLastError());
+            return;
+    }
+}
+
+void WriteToBuffer(VOID)
+{
+   
+    printf("Main thread writing to the shared buffer...\n");
+    buff = "Hello All!!!";
+    if (!SetEvent(ghWriteEvent))
+    {
+        printf("SetEvent failed (%d)\n", GetLastError());
+        return;
+    }
+}
+
+void CloseEvents()
+{
+    CloseHandle(ghWriteEvent);
+}
+
+int main(void)
+{
+    DWORD dwWaitResult;
+    DWORD result = 0;
+
+    CreateEventsAndThreads();
+
+    WriteToBuffer();
+
+    printf("Main thread waiting for reading thread to exit...\n");
+
+    dwWaitResult = WaitForSingleObject(ghThreads, INFINITE);
+
+    switch (dwWaitResult)
+    {
+    case WAIT_OBJECT_0:
+        printf("\nAll threads ended, cleaning up for application exit...\n");
+        break;
+
+    default:
+        printf("WaitForSingleObject failed (%d)\n", GetLastError());
+        result = 1;
+    }
+    // Have single exit with resource cleanup
+    CloseEvents();
+
+    return result;
+}
+
